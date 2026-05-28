282f2a7ede58ffb906e96fab8192267b9e3c42a2
Varakorn <varakorn2324@gmail.com>
2026-05-28 16:19:15 +0700

    asdasd

--- auth/csrfCreator.go
+++ auth/csrfCreator.go
@@ -4,7 +4,9 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"time"
+
 	"github.com/gofiber/fiber/v3"
+	"github.com/varakornpz/providers"
 )
 
 func genCSRFToken(c fiber.Ctx) string {
@@ -16,7 +18,10 @@ func genCSRFToken(c fiber.Ctx) string {
 			Name:     "authstate",
 			Value:    state,
 			Expires:  time.Now().Add(10 * time.Minute),
+			Path:     "/", 
+        	Domain:   providers.AppConf.COOKIEDomain,
 			HTTPOnly: true,
+			SameSite: "Lax",
 			Secure:   false,
 		})
 
